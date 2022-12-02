ef2b9c2791ff6f02f3741e2160d123d394bb7822
Samuel Lee <lee.sam1492@gmail.com>
2022-12-02 14:20:50 -0700

    Renamed test file. Added addistional tests for login page

---
+++ tests/Login.spec.js
@@ -0,0 +1,40 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("Invalid login credentials", async ({ page }) => {
+  await page.goto("https://www.greencompassdev.com/account/login");
+
+  await page.locator("#LoginName").fill("redskytamara");
+  await page.locator("#Password").fill("12346");
+
+  await page.getByRole("button", { name: "Login" }).hover();
+
+  await page.getByRole("button", { name: "Login" }).click();
+  let loginMessage = page.locator("#loginTextContainer");
+  await expect(loginMessage).toHaveText(
+    "Oops! It looks like the username and password combination doesn’t match up with any records in our system. Please try again with a different combination."
+  );
+});
+
+test("Valid Login Credentials", async ({ page }) => {
+  await page.goto("https://www.greencompassdev.com/account/login");
+
+  await page.locator("#LoginName").fill("redskytamara");
+  await page.locator("#Password").fill("123456");
+
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page).toHaveURL(
+    "https://www.greencompassdev.com/account/settings"
+  );
+  await page.goBack();
+  await page.goto("https://www.greencompassdev.com/orders");
+});
+
+test("Join Now Route", async ({ page }) => {
+  await page.goto("https://www.greencompassdev.om/");
+  await page.getByRole("link", { name: "LOGIN" }).click();
+  await page.getByRole("link", { name: "Join Now" }).click();
+  await expect(page).toHaveURL(
+    "https://www.greencompassdev.com/enrollment/become-an-associate"
+  );
+});
