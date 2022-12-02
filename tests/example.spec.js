4b3feb6cf04283c17f1a3423ed1ccc821126af01
Samuel Lee <lee.sam1492@gmail.com>
2022-12-02 14:09:30 -0700

    added test

---
+++ tests/example.spec.js
@@ -0,0 +1,11 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("Join Now Route", async ({ page }) => {
+  await page.goto("https://www.greencompassdev.com/");
+  await page.getByRole("link", { name: "LOGIN" }).click();
+  await page.getByRole("link", { name: "Join Now" }).click();
+  await expect(page).toHaveURL(
+    "https://www.greencompassdev.com/enrollment/become-an-associate"
+  );
+});
