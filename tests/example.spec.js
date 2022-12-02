5b85e5a3097578e053971bc4987f47e1726a22aa
Samuel Lee <lee.sam1492@gmail.com>
2022-12-02 14:14:46 -0700

    creating intentional error

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,7 @@
 const { test, expect } = require("@playwright/test");
 
 test("Join Now Route", async ({ page }) => {
-  await page.goto("https://www.greencompassdev.com/");
+  await page.goto("https://www.greencompassdev.om/");
   await page.getByRole("link", { name: "LOGIN" }).click();
   await page.getByRole("link", { name: "Join Now" }).click();
   await expect(page).toHaveURL(
